725c0079a69d1683c0f6c09026c8f2ec9ae929c5
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-06-29 13:15:05 +0530

    .

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  // origin:"https://shubook-front-end-project.vercel.app/",
-  // credentials:true
+  origin:"http://localhost:3000",
+  credentials:true
 }))
 
 //process .env
